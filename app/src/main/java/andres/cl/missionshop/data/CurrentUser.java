8e3b445c1f81dd8c945daf9f103586d893e60deb
Andrés <andres.murillo.ac@gmail.com>
2017-10-24 16:47:32 -0300

    ADD Magical Camera, ShapeImage

--- app/src/main/java/andres/cl/missionshop/data/CurrentUser.java
+++ app/src/main/java/andres/cl/missionshop/data/CurrentUser.java
@@ -22,4 +22,8 @@ public class CurrentUser {
     public String uid(){
         return currentUser.getUid();
     }
+
+    public String name(){
+        return currentUser.getDisplayName();
+    }
 }
